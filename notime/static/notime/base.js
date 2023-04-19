22b56a1fbd366cfc81187b70aa65b77bb2bf8a39
santacalculus <lchintar@andrew.cmu.edu>
2023-04-19 05:01:15 -0400

    context has num people

--- notime/static/notime/base.js
+++ notime/static/notime/base.js
@@ -1,24 +1,27 @@
 "use strict"
 
-function loadTime(items) {
+function loadTime() {
     let xhr = new XMLHttpRequest()
+    xhr.open("GET", getURL, true)
     xhr.onreadystatechange = function() {
         if (this.readyState != 4) return
-        updatePage(xhr)
+        let line_data = JSON.parse(xhr.responseText);
+        let num_people = line_data.num_people;
+        console.log(num_people);
+        
     }
-    xhr.open("GET", getURL, true)
+    
     console.log("here")
     xhr.send()
 }
 
 function updatePage(xhr) {
     if (xhr.status == 200) {
+        console.log("yes")
         let response = JSON.parse(xhr.responseText)
-        let waitresponse = response.wait_time
-        updateWait(waitresponse)
-        let numresponse = response.num_people
-        updateNum(numresponse)
-        return
+        let numresponse = response.num_people 
+        let lineDiv = document.getElementById("id_line_num")
+        lineDiv.textContent = `${numresponse}`
     }
     if (xhr.status == 0) {
         displayError("Cannot connect to server")
@@ -35,29 +38,6 @@ function updatePage(xhr) {
         displayError(response.error)
         return
     }
-
     displayError(response)
-
 }
 
-function displayError(message) {
-    let errorElement = document.getElementById("error")
-    errorElement.innerHTML = message
-}
-
-function sanitize(s) {
-    // Be sure to replace ampersand first
-    return s.replace(/&/g, '&amp;')
-            .replace(/</g, '&lt;')
-            .replace(/>/g, '&gt;')
-            .replace(/"/g, '&quot;')
-}
-
-
-function updateWait(items) {
-    let item = items[items.length - 1]
-    let waitdiv = document.getElementById("id_predicted_time")
-    let currdate = new Date(item.creation_time)
-    let itemtime = currdate.toLocaleTimeString([], {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit'})
-    waitdiv.innerHTML = itemtime.replace(',', ' ') + '+' + item.wait_time
-}
--- notime/views.py
+++ notime/views.py
@@ -6,13 +6,14 @@ from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 
 from django.utils import timezone
 
 from notime.forms import LoginForm, RegisterForm
 import json
 from django.views.decorators.csrf import ensure_csrf_cookie
+from notime.models import WaitTime, Line
 
 
 # Create your views here.
@@ -83,5 +84,32 @@ def menu_action(request) :
 @login_required
 def laprima_action(request) :
     context = {}
+    #context['wait_time'] = WaitTime.objects.all().order_by('-date_time')
+    line = Line.objects.order_by('-id').first()
+    
+    if line: 
+        
+        #data = {"num_people": line.num_people}
+        line_data = Line.objects.latest('id').num_people
+        
+        #num = json.loads(num_people.content.decode('utf-8'))
+        #context = {'num_people': num['num_people']}
+        context['num_people'] = 3
+        print(context)
+        return render(request, 'notime/laprima.html', context)
+
+    if not 'wait_time' in request.GET or not request.GET['wait_time'] or not 'num_people' in request.GET or not request.GET['num_people']:
+        return _my_json_error_response("You must enter a comment to add.", status=400)
     if request.method == 'GET' :
         return render(request, 'notime/laprima.html', context)
+    if request.method == 'POST' :
+        return render(request, 'notime/laprima.html', context)
+
+
+
+def _my_json_error_response(message, status=200):
+    # You can create your JSON by constructing the string representation yourself (or just use json.dumps)
+    response_json = '{ "error": "' + message + '" }'
+    return HttpResponse(response_json, content_type='application/json', status=status)
+
+    
